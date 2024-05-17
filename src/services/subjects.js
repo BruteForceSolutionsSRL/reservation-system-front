9471487df508ed3b5607f8f7f105eebb910a4479
Sega1A <sergio.garciacuchallo@gmail.com>
2024-05-17 10:42:55 -0400

    fix: fixed export error.

--- src/services/subjects.js
+++ src/services/subjects.js
@@ -1,6 +1,6 @@
 const url = import.meta.env.VITE_REACT_API_URL;
 
-export default function getSubjects() {
+export function getSubjects() {
   return fetch(url + `subjects/teacher/${2}`)
     .then((response) => response.json())
     .then((data) => {

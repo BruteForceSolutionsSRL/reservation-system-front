1a003a661e819bbc4ef08458de8e157b9b78230b
erwin <erwin0pisis@gmail.com>
2024-06-04 22:07:26 -0400

    availability advance, bug fixes

--- src/Pages/Environment/Disponibility.jsx
+++ src/Pages/Environment/Disponibility.jsx
@@ -1,163 +1,474 @@
-import React, { useState } from "react";
-import TableDinamic from "../../Components/Table/Table.jsx";
-import Blocks from "../../Components/Environment/Blocks.jsx";
-import Classrooms from "../../Components/Classrooms/Classrooms.jsx";
+import { useState, useEffect } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { Table } from "react-bootstrap";
+import Modal from "react-bootstrap/Modal";
+import Spinner from "react-bootstrap/Spinner";
+import "./Disponibility.css";
 
-export default function Disponibility() {
-  let environments = [
+function Disponibility() {
+  const URL = import.meta.env.VITE_REACT_API_URL;
+  const [block, setBlock] = useState([]);
+  const [periods, setPeriods] = useState([]);
+  const [classrom, setClassrom] = useState([]);
+  const [selectedClassrooms, setSelectedClassrooms] = useState([]);
+
+  const [formData, setFormData] = useState({
+    date: "",
+    block_id: "",
+    classroom_id: [],
+    time_slot_id: [],
+  });
+
+  // erros form
+  const [errors, setErrors] = useState({
+    date: "",
+    block_id: "",
+    classroom_id: [],
+    time_slot_id: [],
+  });
+
+  const environments = [
     {
-      classroom_name: "Auditorio",
-      colors: [
-        {
-          id: 1,
-          color: "#FFD8A8",
-        },
-        {
-          id: 14,
-          color: "#FFD8A8",
-        },
-        {
-          id: 15,
-          color: "#FFC9C9",
-        },
-        {
-          id: 16,
-          color: "#FFC9C9",
-        },
-        {
-          id: 21,
-          color: "#FFD8A8",
-        },
-      ],
-    },
-    {
-      classroom_name: "693A",
-      colors: [],
-    },
-    {
-      classroom_name: "693B",
-      colors: [],
-    },
-    {
-      classroom_name: "693C",
-      colors: [],
-    },
-    {
-      classroom_name: "693D",
-      colors: [
-        {
-          id: 2,
-          color: "#FFD8A8",
-        },
-        {
-          id: 3,
-          color: "#FFD8A8",
-        },
-        {
-          id: 4,
-          color: "#FFD8A8",
-        },
-        {
-          id: 5,
-          color: "#FFC9C9",
-        },
-      ],
-    },
-    {
-      classroom_name: "693F",
-      colors: [
-        {
-          id: 6,
-          color: "#FFD8A8",
-        },
-        {
-          id: 7,
-          color: "#FFD8A8",
-        },
-        {
-          id: 8,
-          color: "#FFD8A8",
-        },
-        {
-          id: 10,
-          color: "#FFC9C9",
-        },
-      ],
-    },
-    {
-      classroom_name: "692A",
-      colors: [],
-    },
-    {
-      classroom_name: "692B",
-      colors: [],
-    },
-    {
-      classroom_name: "692C",
-      colors: [],
-    },
-    {
-      classroom_name: "692D",
-      colors: [],
-    },
-    {
-      classroom_name: "692E",
-      colors: [],
-    },
-    {
-      classroom_name: "692F",
-      colors: [],
-    },
-    {
-      classroom_name: "691A",
-      colors: [],
-    },
-    {
-      classroom_name: "691B",
-      colors: [],
+      classroom_name: "690A",
+      "06:45:00": {
+        valor: 0,
+        message: "Disponible",
+      },
+      "07:30:00": {
+        valor: 0,
+        message: "Disponible",
+      },
+      "08:15:00": {
+        valor: 0,
+        message: "Disponible",
+      },
+      "09:00:00": {
+        valor: 0,
+        message: "Disponible",
+      },
+      "09:45:00": {
+        valor: 0,
+        message: "Disponible",
+      },
+      "10:30:00": {
+        valor: 0,
+        message: "Disponible",
+      },
+      "11:15:00": {
+        valor: 0,
+        message: "Disponible",
+      },
+      "12:00:00": {
+        valor: 0,
+        message: "Disponible",
+      },
+      "12:45:00": {
+        valor: 0,
+        message: "Disponible",
+      },
+      "13:30:00": {
+        valor: 0,
+        message: "Disponible",
+      },
+      "14:15:00": {
+        valor: 0,
+        message: "Disponible",
+      },
+      "15:00:00": {
+        valor: 0,
+        message: "Disponible",
+      },
+      "15:45:00": {
+        valor: 0,
+        message: "Disponible",
+      },
+      "16:30:00": {
+        valor: 0,
+        message: "Disponible",
+      },
+      "17:15:00": {
+        valor: 0,
+        message: "Disponible",
+      },
+      "18:00:00": {
+        valor: 0,
+        message: "Disponible",
+      },
+      "18:45:00": {
+        valor: 0,
+        message: "Disponible",
+      },
+      "19:30:00": {
+        valor: 0,
+        message: "Disponible",
+      },
+      "20:15:00": {
+        valor: 0,
+        message: "Disponible",
+      },
+      "21:00:00": {
+        valor: 0,
+        message: "Disponible",
+      },
+      "21:45:00": {
+        valor: 0,
+        message: "Disponible",
+      },
     },
   ];
 
-  let blocks = [
-    {
-      block_id: 0,
-      block_name: "Edificio nuevo",
-      block_maxfloor: 0,
-    },
-  ];
+  const getColor = (valor) => {
+    if (valor === 0) {
+      return "#B2F2BB";
+    } else if (valor === 1) {
+      return "#FF9E9E";
+    } else {
+      return "#FFDFA3";
+    }
+    //return valor === 0 ? "#B2F2BB" : "#FF6B6B"; // disponible y ocupado
+  };
 
-  let classrooms = [
-    {
-      classroom_id: 0,
-      classroom_name: "Auditorio",
-      capacity: 0,
-      floor_number: 0,
-    },
-    {
-      classroom_id: 0,
-      classroom_name: "693A",
-      capacity: 0,
-      floor_number: 0,
-    },
-    {
-      classroom_id: 0,
-      classroom_name: "693B",
-      capacity: 0,
-      floor_number: 0,
-    },
-  ];
+  //Consulta a backEnd
+  const fetchData = async (endpoint, setterFunction) => {
+    try {
+      const response = await fetch(URL + endpoint);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      setterFunction(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData(`blocks`, setBlock);
+    fetchData(`timeslots`, setPeriods);
+  }, []);
+
+  //validator of DATE
+  const validateDate = (value) => {
+    if (!value || value.trim() === "") {
+      return "Seleccione una fecha.";
+    }
+    return null;
+  };
+
+  //validator of BOCK
+  const validateBlock = (value) => {
+    if (!value || value.trim() === "") {
+      return "Seleccione un bloque.";
+    }
+    return null;
+  };
+  //validator of CLASSROM
+  const validateClassroom = (value) => {
+    if (!value || value.length === 0) {
+      return "Seleccione al menos un aula.";
+    }
+    return null;
+  };
+
+  //Boton enviar solicitud FORM
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let newErrors = {};
+    newErrors.date = validateDate(formData.date);
+    newErrors.block_id = validateBlock(formData.block_id);
+    newErrors.classroom_id = validateClassroom(formData.classroom_id);
+    setErrors(newErrors);
+
+    if (
+      !newErrors.date &&
+      !newErrors.block_id &&
+      !newErrors.classroom_id
+    ) {
+      console.log("Datos del formulario", formData);
+    } else {
+      console.log("LLENE TODOS LOS CAMPOS DEL FORMULARIO");
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    console.log(name, value);
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+    const error = validators[name](value);
+    setErrors({
+      ...errors,
+      [name]: error,
+    });
+  };
+
+  const handleTimeSlotChange = (name, value) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      time_slot_id: {
+        ...prevData.time_slot_id,
+        [name]: value,
+      },
+    }));
+    const error = validateTimeSlot({
+      ...formData.time_slot_id,
+      [name]: value,
+    });
+    setErrors({
+      ...errors,
+      time_slot_id: error,
+    });
+  };
+
+  const filteredEndPeriods = periods.filter(
+    (period) =>
+      !formData.time_slot_id.start ||
+      period.time_slot_id > formData.time_slot_id.start
+  );
+
+  const validators = {
+    date: validateDate,
+    block_id: validateBlock,
+    classroom_id: validateClassroom,
+    // Agrega más validadores para otros campos si es necesario
+  };
+
+  // update table AULA -- block_id
+  useEffect(() => {
+    if (!formData.block_id) {
+      setClassrom([]);
+      setSelectedClassrooms([]);
+      setFormData((prevData) => ({
+        ...prevData,
+        classroom_id: [],
+      }));
+    } else {
+      fetchData(`classrooms/block/${formData.block_id}`, setClassrom);
+    }
+  }, [formData.block_id]);
+
+  //Aulas
+  const handleSelectClassroom = (id) => {
+    setSelectedClassrooms((prevSelected) =>
+      prevSelected.includes(id)
+        ? prevSelected.filter((classroomId) => classroomId !== id)
+        : [...prevSelected, id]
+    );
+    setFormData((prevData) => {
+      const newClassroomIds = selectedClassrooms.includes(id)
+        ? prevData.classroom_id.filter((classroomId) => classroomId !== id)
+        : [...prevData.classroom_id, id];
+      return {
+        ...prevData,
+        classroom_id: newClassroomIds,
+      };
+    });
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      classroom_id: validateClassroom(
+        selectedClassrooms.includes(id)
+          ? formData.classroom_id.filter((classroomId) => classroomId !== id)
+          : [...formData.classroom_id, id]
+      ),
+    }));
+  };
+  //console.log("Datos del formulario desde afuera", formData);
 
   return (
-    <>
-      <div className="content-fluid">
-        <h1 className="text-center">Disponibilidad de ambiente</h1>
-        <div>
-          <Classrooms blocks={blocks} classrooms={classrooms} />
-        </div>
-        <div>
-          <TableDinamic environments={environments} range={[0, 16]} />
-        </div>
+    <div className="table-responsive">
+      <h1 className="text-center">Disponibilidad de ambientes</h1>
+
+      <div className="mt-5">
+        <Form noValidate onSubmit={handleSubmit}>
+          <div className="tag-container position-relative mb-3">
+            <label className="tag-label">Ambiente</label>
+            <div>
+              <label className="fw-bold">BLOQUE</label>
+              <Form.Select
+                className="mt-2"
+                type="input"
+                name="block_id"
+                value={formData.block_id}
+                onChange={handleChange}
+                isInvalid={!!errors.block_id}
+              >
+                <option value="">Seleccione un bloque</option>
+                {block.map((option) => (
+                  <option key={option.block_id} value={option.block_id}>
+                    {option.block_name}
+                  </option>
+                ))}
+              </Form.Select>
+              <Form.Control.Feedback type="invalid">
+                {errors.block_id}
+              </Form.Control.Feedback>
+            </div>
+
+            <label className="fw-bold mt-3">AULAS</label>
+            <div className="scrol-teacher-modal">
+              <Table
+                striped
+                bordered
+                hover
+                className="table-tag text-center mt-2"
+              >
+                <thead>
+                  <tr>
+                    <th
+                      className="sticky-header text-center"
+                      style={{
+                        backgroundColor: "rgb(4, 94, 140)",
+                        color: "white",
+                        userSelect: "none",
+                      }}
+                    >
+                      Nombre
+                    </th>
+                    <th
+                      className="sticky-header text-center"
+                      style={{
+                        backgroundColor: "rgb(4, 94, 140)",
+                        color: "white",
+                        userSelect: "none",
+                      }}
+                    >
+                      Capacidad
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {classrom.map((item) => (
+                    <tr
+                      key={item.classroom_id}
+                      onClick={() => handleSelectClassroom(item.classroom_id)}
+                      className={
+                        selectedClassrooms.includes(item.classroom_id)
+                          ? "table-primary"
+                          : ""
+                      }
+                      style={{ cursor: "pointer" }}
+                    >
+                      <td style={{ userSelect: "none" }}>
+                        {item.classroom_name}
+                      </td>
+                      <td style={{ userSelect: "none" }}>{item.capacity}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            </div>
+            <div className="invalid-feedback d-block">
+              {errors.classroom_id}
+            </div>
+          </div>
+          <div className="tag-container position-relative mb-3">
+            <label className="tag-label">Periodo</label>
+            <div>
+              <label className="fw-bold">PERIODO INICIAL</label>
+              <Form.Select
+                className="mt-2"
+                value={formData.time_slot_id.start}
+                onChange={(e) => handleTimeSlotChange("start", e.target.value)}
+              >
+                {periods.map((period) => (
+                  <option key={period.time_slot_id} value={period.time_slot_id}>
+                    {period.time}
+                  </option>
+                ))}
+              </Form.Select>
+              <label className="fw-bold mt-2">PERIODO FINAL</label>
+              <Form.Select
+                className="mt-2"
+                value={formData.time_slot_id.end}
+                onChange={(e) => handleTimeSlotChange("end", e.target.value)}
+              >
+                {filteredEndPeriods.map((period) => (
+                  <option key={period.time_slot_id} value={period.time_slot_id}>
+                    {period.time}
+                  </option>
+                ))}
+              </Form.Select>
+            </div>
+          </div>
+          <div>
+            <label className="fw-bold">FECHA</label>
+            <Form.Control
+              className="mt-2"
+              type="date"
+              name="date"
+              value={formData.date}
+              onChange={handleChange}
+              isInvalid={!!errors.date}
+            ></Form.Control>
+            <Form.Control.Feedback type="invalid">
+              {errors.date}
+            </Form.Control.Feedback>
+          </div>
+          <div className="mt-3">
+            <Button type="submit">Verificar</Button>
+          </div>
+        </Form>
       </div>
-    </>
+
+      <div className="table-global mt-3">
+        <table className="table">
+          <thead>
+            <tr>
+              <th
+                className="static-header-cell"
+                style={{
+                  backgroundColor: "#E7E7E7",
+                  color: "black",
+                }}
+              >
+                Ambiente
+              </th>
+              {environments.map((env, index) => (
+                <th
+                  className="environment-data"
+                  key={index}
+                  style={{
+                    backgroundColor: "#6DC2F5",
+                    color: "black",
+                  }}
+                >
+                  {env.classroom_name}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {periods.map((period, periodIndex) => (
+              <tr key={periodIndex}>
+                <td
+                  className="time-cell"
+                  style={{
+                    backgroundColor: "#E7E7E7",
+                    color: "black",
+                  }}
+                >
+                  {period.time}
+                </td>
+                {environments.map((env, envIndex) => (
+                  <td
+                    key={envIndex}
+                    className="data-cell"
+                    style={{
+                      backgroundColor: getColor(env[period.time].valor),
+                    }}
+                  >
+                    {env[period.time].message}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 }
+export default Disponibility;

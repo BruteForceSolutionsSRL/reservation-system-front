0a9e90f2a6f57ced79c1faed0e3085a8d4afe439
erwin <erwin0pisis@gmail.com>
2024-06-24 21:41:07 -0400

    homepage

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import ErrorPage from "./Components/ErrorPage/ErrorPage";
 import LandingPage from "./Pages/LandingPage/LandingPage";
 import UserRoutes from "./routes/UserRoutes";
 import SuperUserRoutes from "./routes/SuperUserRoutes";
+import Home from "./Pages/User/Homepage/Home";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route index path="/" element={<Home />} />
         <Route index path="/" element={<LandingPage />} />
         <Route path="/user/*" element={<UserRoutes />} />
         <Route path="/superuser/*" element={<SuperUserRoutes />} />

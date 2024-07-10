2add424ba8c5a0f70b9d8f60f362b95de9e68f7b
erwin <erwin0pisis@gmail.com>
2024-07-10 17:48:31 -0400

    refactor

--- src/Pages/Environment/Disponibility.jsx
+++ src/Pages/Environment/Disponibility.jsx
@@ -180,7 +180,7 @@ function Disponibility() {
   };
 
   return (
-    <div className="m-3">
+    <div className="m-4">
       <h1 className="text-center">Disponibilidad de ambientes</h1>
 
       <div className="mt-3 ms-3 me-3">
